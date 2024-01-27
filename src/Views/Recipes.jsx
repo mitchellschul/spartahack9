0ede84ed933f9337fd2e623d9e6c086c9a5fba75
Isaac Luebke <luebkeis@msu.edu>
2024-01-27 17:44:19 -0500

    Recipe Page

--- src/Views/Recipes.jsx
+++ src/Views/Recipes.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import "../../src/Css/recipe.css"
 
 const Recipes = () => {
     return (
-        <div>Yagga</div>
+        <div className="App">
+      <header>
+        <h4>Recipes</h4>
+      </header>
+
+      <div className="container">
+        <div className="image">
+          <img></img>
+        </div>
+        <div className="content">
+          <h5>Welcome to Our Website</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut elit auctor, placerat velit nec, vestibulum velit. Donec convallis eget velit ac lobortis.</p>
+        </div>
+        <div className="centered-text">
+          <p>More information about the recipe, lorem ipsum etc.</p>
+        </div>
+      </div>
+    </div>
 
     )
 }
